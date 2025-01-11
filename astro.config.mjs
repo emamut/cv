b51adc50455e5f8f30363f91eaa5c770f61834df
Faber Vergara <fabervergara@gmail.com>
2025-01-11 11:11:41 -0500

    feat: add github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://emamut.vercel.app/',
+  site: 'https://emamut.github.io/',
+  base: 'github.com/emamut/cv',
   integrations: [
     partytown({
       config: {
